a468bfd8ca88d135ffe15ba6c799101db8c0af32
Lonkin <valerian.cin@hotmail.fr>
2017-01-19 08:12:32 +0100

    Update analyse_syntaxique.cc

--- Analyse_syntaxique/source/analyse_syntaxique.cc
+++ Analyse_syntaxique/source/analyse_syntaxique.cc
@@ -24,6 +24,37 @@
 
 using namespace std;
 
+#define N 50
+#include <string>
+
+using namespace std;
+
+/*string keywordList[N] = {
+	"library",
+	"use",
+	"all",	
+	"entity",
+	"is",
+	"port",
+	"in",
+	"out",
+	"downto",
+	"upto",
+	"end",
+ 	"architecture",
+	"of",
+	"signal",
+	"variable",
+	"begin",
+	"process",
+	"case",
+	"when",
+	"others",
+	"if",
+	"then",
+	"else",
+	"elsif"	
+};*/
 
 //tree<string> get_arbre_primaire(const list<lexeme> & l){
 
